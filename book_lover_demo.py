dff6c4d927352f940085ba5990cc6373d6dd0e92
aubrwing <98354671+aubrwing@users.noreply.github.com>
2022-07-12 08:16:23 -0400

    Add files via upload

---
+++ book_lover_demo.py
@@ -0,0 +1,4 @@
+from booklover.booklover import BookLover
+bookList1 = BookLover("Aubrey","alw8ef@virginia.edu","Science Fiction")
+bookList1.add_book("Harry Potter",5)
+print(bookList1.num_books_read())
